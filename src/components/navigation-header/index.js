000f463dd528ddb2b891c869d0708bbea2242250
ivevasiljevic <ive@profico.hr>
2020-10-22 10:58:51 +0200

    add navigation

--- src/components/navigation-header/index.js
+++ src/components/navigation-header/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 import styles from './style.module.css'
 import Logo from '../logo'
+import Navigation from '../navigation'
 
 const NavigationHeader = () => (
   <section className={styles.navigationHeader}>
     <Logo />
+    <Navigation />
   </section>
 )
 export default NavigationHeader
---
+++ src/components/navigation/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import NavigationBar from '../navigation-bar'
+import Combobox from '../combobox'
+import styles from './style.module.css'
+
+const Navigation = () => (
+  <section>
+    <NavigationBar />
+    <Combobox />
+  </section>
+)
+
+export default Navigation
