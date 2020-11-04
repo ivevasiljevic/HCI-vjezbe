4b42565e2939b8156e92c8f55873077d24c299ae
ivevasiljevic <ive@profico.hr>
2020-11-04 19:18:24 +0100

    create page-content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
  
 import HeaderFooterLayout from "../layouts/headerFooter"
+import PageContent from "../modules/content-page"
 import TitlePage from '../modules/title-page'
  
 const IndexPage = () => (
   <HeaderFooterLayout activeTab="Home">
     <TitlePage/>
+    <PageContent/>
   </HeaderFooterLayout>
 )
  
