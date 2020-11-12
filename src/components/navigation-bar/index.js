5cd2529881f981a0232d927b94591b83e794c78a
ivevasiljevic <ive@profico.hr>
2020-11-12 10:55:51 +0100

    contact page init

--- src/components/navigation-bar/index.js
+++ src/components/navigation-bar/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 import styles from './style.module.css'
 import { navs as navTabs } from '../../constants/const'
+import { Link } from 'gatsby'
 
 const NavigationBar = ({ activeTab, useThisStyle }) => (
   <nav className={styles[useThisStyle || 'navigationBar']}>
-      {navTabs.map(tab => <li className={tab=== activeTab ? styles.active : ''}>
-          {tab}</li>
+      {navTabs.map(({tab, to}) => (
+        <Link to={to} >
+            <li className={tab === activeTab ? styles.active : ''}>{tab}</li>
+        </Link>)
       )}
   </nav>
 )
