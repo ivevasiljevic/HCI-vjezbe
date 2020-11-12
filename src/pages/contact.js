a350dc5458958e091689cac445d264a711621503
ivevasiljevic <ive@profico.hr>
2020-11-12 11:00:11 +0100

    contact page contact-form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,12 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
- 
+import ContactSeparator from '../modules/contact-separator'
+import ContactForm from '../modules/contact-form' 
+
 const ContactPage = () => (
     <HeaderFooterLayout activeTab="Contact">
+        <ContactSeparator />
+        <ContactForm />
     </HeaderFooterLayout>
 )
  
